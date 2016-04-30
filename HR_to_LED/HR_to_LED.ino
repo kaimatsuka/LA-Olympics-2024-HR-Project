9f301d9351fa2bdfb7ba0a0e210900b1b58788ca
Kai Matsuka <kai.matsuka1@gmail.com>
2016-04-30 07:35:48 -0700

    First Commit! :D
    This is the version of the arduino code that Don and Jason worked on.

---
+++ HR_to_LED/HR_to_LED.ino
@@ -0,0 +1,58 @@
+// Pulse Monitor Test Script
+int sensorPin = 0;
+double alpha = 0.75;
+int period = 50;
+double change = 0.0;
+double minval = 0.0;
+int redpin = 11; // select the pin for the red LED
+int bluepin = 10; // select the pin for the blue LED
+int greenpin = 9 ;// select the pin for the green LED
+int val;
+
+/* 
+ *  11 = R, 10 = G, 9 = B
+ *  
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (redpin, OUTPUT);
+  pinMode (bluepin, OUTPUT);
+  pinMode (greenpin, OUTPUT);
+  Serial.begin (9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:5
+  static double oldValue = 0;
+  static double oldChange = 0;
+  static int count = 0;
+  static double total = 0;
+ 
+  int rawValue = analogRead (sensorPin);
+  double value = alpha * oldValue + (1 - alpha) * rawValue;
+  total += value;
+  count++;
+  if (count == 50) {
+    double averageValue = total / count;
+    Serial.println(averageValue);
+    total = 0;
+    count = 0;
+    if (averageValue >= 500 && averageValue <= 650) {
+     analogWrite(11, 0);
+      analogWrite (10, 255);
+    } else {
+      analogWrite(10, 0);
+      analogWrite(11, 255);
+    }
+   }
+//
+//  Serial.print (rawValue);
+//  Serial.print (",");
+//  Serial.println (value);
+  
+//  for (val = 255; val> 0; val --)
+//  {
+  oldValue = value;
+  delay (period);
+}
