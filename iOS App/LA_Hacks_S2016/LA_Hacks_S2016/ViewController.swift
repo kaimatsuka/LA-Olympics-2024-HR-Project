599dd44aa1eaacdb04ccdd0602d6fbf2dfa28199
missannylin <annylin94@Annys-MacBook-Pro.local>
2016-05-01 06:00:29 -0700

    Backup for Beautiful APP IU
    Backup for our beautiful app iu geometry and image

---
+++ iOS App/LA_Hacks_S2016/LA_Hacks_S2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LA_Hacks_S2016
+//
+//  Created by Anny Lin on 5/1/16.
+//  Copyright © 2016 Anny Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
